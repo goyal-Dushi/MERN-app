d8fbc5a82f3273f3ff26c21651728bd3ff36a506
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-15 21:28:26 +0530

    added response msg to show alert and optimized code

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,55 +1,72 @@
-const router = require('express').Router();
-let Exercise = require('../models/exercise.model');
+const router = require("express").Router();
+let Exercise = require("../models/exercise.model");
 
-router.route('/').get((req,res) => {
-    Exercise.find()
-    .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json("Error : "+err));
-})
-
-router.route('/add').post((req,res) => {
-    const uname = req.body.username;
-    const desc = req.body.description;
-    const dur = Number(req.body.duration);
-    const time = Date.parse(req.body.date);
-
-    const newExercise = new Exercise({
-        username: uname,
-        description : desc,
-        duration : dur,
-        date : time
-    });
-
-    newExercise.save()
-    .then(() => res.json('Exercise added'))
-    .catch(err => res.status(400).json('Error :'+err));
-
-})
-
-router.route('/:id').get((req,res) => {
-    Exercise.findById(req.params.id)
-    .then(exercises => res.json(exercises))
-    .catch(err => res.status(400).json("Error : "+err));
-})
-.delete((req,res) => {
-    Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Exercises deleted"))
-    .catch(err => res.status(400).json("Error : "+err)); 
+router.route("/").get((req, res) => {
+  Exercise.find()
+    .then((exercises) => res.json(exercises))
+    .catch((err) => res.status(400).json("Error : " + err));
 });
 
-router.route('/update/:id').patch((req,res) => {
-    Exercise.findByIdAndUpdate(req.params.id)
-    .then(exercise => {
-        exercise.username = req.body.username;
-        exercise.description = req.body.description;
-        exercise.duration = Number(req.body.duration);
-        exercise.date = Date.parse(req.body.date);
+router.route("/add").post((req, res) => {
+  const newExercise = new Exercise(req.body);
+  newExercise
+    .save()
+    .then(() =>
+      res.json({
+        show: true,
+        msg: "New Exercise routine created Successfully.",
+        type: "success",
+      })
+    )
+    .catch((err) =>
+      res.status(400).json("Error while adding new exercise:" + err)
+    );
+});
 
-        exercise.save()
-        .then(() => res.json("Exercise Updated"))
-        .catch(err => res.status(400).json("Error : "+err));
-    })
-    .catch(err => res.status(400).json("Error : "+err));
+router
+  .route("/:id")
+  .get((req, res) => {
+    Exercise.findById(req.params.id)
+      .then((exercises) => res.json(exercises))
+      .catch((err) =>
+        res.status(400).json("Error while fetching exercise by ID : " + err)
+      );
+  })
+  .delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json("Exercises deleted"))
+      .catch((err) =>
+        res.status(400).json("Error while deleting exercise : " + err)
+      );
+  });
+
+router.route("/update/:id").patch((req, res) => {
+  Exercise.findByIdAndUpdate(req.params.id).then((exercise) => {
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = Number(req.body.duration);
+    exercise.date = Date.parse(req.body.date);
+    // exercise = req.body;
+    exercise
+      .save()
+      .then(() =>
+        res.json({
+          show: true,
+          msg: "Exercise Updated Successfully!",
+          type: "success",
+        })
+      )
+      .catch((err) => {
+        console.log("Error Updating: ", err);
+        return res
+          .status(400)
+          .json({
+            show: true,
+            msg: "Error occured while updating exercise, please try again later.",
+            type: "danger",
+          });
+      });
+  });
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,23 +1,32 @@
 // jshint esversion:6
 
-const router = require('express').Router();
+const router = require("express").Router();
 let User = require("../models/user.model");
 
-router.route('/').get((req,res) => {
-    User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error : '+err));
+router.route("/").get((req, res) => {
+  User.find()
+    .then((users) => res.json(users))
+    .catch((err) => res.status(400).json("Error : " + err));
 });
 
-router.route('/add').post((req,res) => {
-    const uname = req.body.username;
-    const newUser = new User({
-        username: uname
-    });
+router.route("/add").post((req, res) => {
+  const uname = req.body.username;
+  const newUser = new User({
+    username: uname,
+  });
 
-    newUser.save()
-    .then(() => res.json('User Added'))
-    .catch(err => res.status(400).json("Error : "+err));
+  newUser
+    .save()
+    .then(() =>
+      res.json({
+        show: true,
+        msg: "New user has been created Successfully!",
+        type: "success",
+      })
+    )
+    .catch((err) =>
+      res.status(400).json("Error while creating new User: " + err)
+    );
 });
 
 module.exports = router;
