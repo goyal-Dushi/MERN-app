60f7e6dee1f15e7c512243a228222d47bcb198ea
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-15 21:24:20 +0530

    added bootstrap alert and updated routes to pass props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import ExercisesList from './components/exercisesList';
-import EditExercise from './components/editExercise';
-import CreateExercise from './components/createExercise';
-import CreateUser from './components/createUser';
-import Navbar from './components/navbar';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
- 
+import ExercisesList from "./components/exercisesList";
+import EditExercise from "./components/editExercise";
+import CreateExercise from "./components/createExercise";
+import CreateUser from "./components/createUser";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useState } from "react";
+
 function App() {
+  const [showAlert, setAlert] = useState({ show: false, msg: "", type: "" });
+
   return (
     <Router>
-    <div className="container" > 
-    <Navbar/>
-    <div className="App">
-      <Route path='/' exact component={ExercisesList} />
-      <Route path='/edit/:id' component={EditExercise} />
-      <Route path='/create' component={CreateExercise} />
-      <Route path='/user' component={CreateUser} />
-    </div>
-    </div>
+      <div className='container-fluid'>
+        <Navbar />
+        {showAlert.show ? (
+          <div
+            className={`alert alert-${showAlert.type} alert-dismissible fade show mt-5`}
+            role='alert'
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}>
+            {showAlert.msg}
+            <button
+              style={{ width: "20px" }}
+              type='button'
+              className='close'
+              data-dismiss='alert'
+              aria-label='Close'
+              onClick={() => setAlert({ show: false, msg: "", type: "" })}>
+              <span aria-hidden={"true"}>&times;</span>
+            </button>
+          </div>
+        ) : null}
+        <div className='App'>
+          <Route path='/' exact component={ExercisesList} />
+          <Route path='/update/:id'>
+            <EditExercise setPopup={setAlert} />
+          </Route>
+          <Route path='/create'>
+            <CreateExercise setPopup={setAlert} />
+          </Route>
+          <Route path='/user'>
+            <CreateUser setPopup={setAlert} />
+          </Route>
+        </div>
+      </div>
     </Router>
   );
 }
