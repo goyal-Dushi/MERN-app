38392798a437dc48f229d517d181d0143afce663
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-15 21:27:23 +0530

    updated username min length to 3

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,19 +1,22 @@
 // jshint esversion:6
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        minlength:4
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
     },
-},{
-        timestamps: true,
-});
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('user', userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
