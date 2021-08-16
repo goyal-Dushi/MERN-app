3743ae5c2a72d035191d99601501ef93ab0a0392
goyal-Dushi <dushyantgoyal28@gmail.com>
2021-08-16 14:16:52 +0530

    added btn toggler for smalller widths

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,24 @@ import { Link } from "react-router-dom";
 function navbar() {
   return (
     <nav
-      className='navbar navbar-dark bg-dark navbar-expand-sm fixed-top'
+      className='navbar navbar-dark bg-dark navbar-expand-sm fixed-top p-3 mb-5'
       style={{ padding: "0px 10px" }}>
       <Link to='/' className='navbar-brand'>
         Exercise Tracker
       </Link>
-      <div className='collapse navbar-collapse justify-content-end'>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navContent'
+        aria-controls='navContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'>
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div
+        className='collapse navbar-collapse justify-content-end'
+        id={"navContent"}>
         <ul className='navbar-nav mr-auto'>
           <li className='navbar-item'>
             <Link to='/' className='nav-link'>
